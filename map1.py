01b22c61c92a558649f04201cc6a65ab06ed0cc1
Kinga <kinga.maleszewska@o2.pl>
2024-11-06 13:40:40 +0100

    Customize popups

--- map1.py
+++ map1.py
@@ -4,13 +4,22 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+html = """
+    Volcano name:<br>
+    <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+    Height: %s m
+"""
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="cartodb positron")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    map.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el, nam in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (nam, nam, el), width=200, height=100)
+    map.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
